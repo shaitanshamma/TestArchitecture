6d068e018032a9f9cc1401285de39e625d4f78e9
shaitanshamma <kurbatov_andr@hotmail.com>
2020-04-21 09:23:04 +0300

    add inner Builder class

--- src/main/java/lesson3/Main.java
+++ src/main/java/lesson3/Main.java
@@ -2,6 +2,7 @@ package lesson3;
 
 public class Main {
     public static void main(String[] args) {
-
+        Sticker sticker = new Sticker.Builder("KPMF").glueBrand("3M").build();
+        sticker.info();
     }
 }
